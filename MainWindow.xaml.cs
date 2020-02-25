eafa80f396e1044ff68ab958d1b89c88336bf8da
Alexandr Voinkov <karauldouble@gmail.com>
2020-02-26 01:35:29 +0700

    Binding treeview for structureProject

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -17,28 +18,32 @@ namespace Hiker_Editor
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary> 
     public partial class MainWindow : Window
     {
+        ObservableCollection<Node> nodes;
         public MainWindow()
         {
             InitializeComponent();
+
+        nodes = new ObservableCollection<Node>
+        {
+            new Node { Name="Sprites" },
+            new Node { Name="Scripts" },
+            new Node { Name="Objects" },
+            new Node { Name="Rooms" }
+        };
+        StructureProject.ItemsSource = nodes;
         }
 
         private void CommonCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = true;
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            foreach (TreeViewItem item in StructureProject.Items)
-            {
-                if(item.Header as string == "Rooms")
-                {
-                    item.Items.Add("room");
-                }
-            }
-        }
+    }
+    public class Node
+    {
+        public string Name { get; set; }
+        public ObservableCollection<Node> Nodes { get; set; }
     }
 }
