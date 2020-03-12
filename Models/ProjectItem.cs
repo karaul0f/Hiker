2422f1bbbff03d76fd24eb1627b8dea791d2ad22
Alexandr Voinkov <karauldouble@gmail.com>
2020-03-13 01:29:16 +0700

    binding icons structProj

--- Models/ProjectItem.cs
+++ Models/ProjectItem.cs
@@ -18,29 +18,39 @@ namespace Hiker_Editor.Models
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
+        private string _imagePath;
+        public string ImagePath
+        {
+            get { return _imagePath; }
+            set
+            {
+                if (value == _imagePath) return;
+                _imagePath = value;
+                OnPropertyChanged();
+            }
+        }
         private string _name;
         public string Name
-        { get { return _name; }
-          set
+        { 
+            get { return _name; }
+            set
             {
                 if (value == _name) return;
                 _name = value;
                 OnPropertyChanged();
             }
         }
-
         public static ObservableCollection<ProjectItem> InitializationStructureProject()
         {
             ObservableCollection<ProjectItem> structureProject = new ObservableCollection<ProjectItem>
             {
-                new ProjectItem { Name="Sprites", ItemsOperation = new ObservableCollection<MenuItem> { new MenuItem { Header = "Add sprite" } } },
-                new ProjectItem { Name="Scripts", ItemsOperation = new ObservableCollection<MenuItem> { new MenuItem { Header = "Add script" } } },
-                new ProjectItem { Name="Objects", ItemsOperation = new ObservableCollection<MenuItem> { new MenuItem { Header = "Add object" } } },
-                new ProjectItem { Name="Rooms", ItemsOperation = new ObservableCollection<MenuItem> { new MenuItem { Header = "Add room" } } }
+                new ProjectItem { Name="Sprites", ImagePath = "/Images/folder.png", ItemsOperation = new ObservableCollection<MenuItem> { new MenuItem { Header = "Add sprite" } } },
+                new ProjectItem { Name="Scripts", ImagePath = "/Images/folder.png", ItemsOperation = new ObservableCollection<MenuItem> { new MenuItem { Header = "Add script" } } },
+                new ProjectItem { Name="Objects", ImagePath = "/Images/folder.png", ItemsOperation = new ObservableCollection<MenuItem> { new MenuItem { Header = "Add object" } } },
+                new ProjectItem { Name="Rooms", ImagePath = "/Images/folder.png", ItemsOperation = new ObservableCollection<MenuItem> { new MenuItem { Header = "Add room" } } }
             };
             return structureProject;
         }
-
         public ObservableCollection<MenuItem> ItemsOperation { get; set; }
         public ObservableCollection<ProjectItem> Items { get; set; }
     }
