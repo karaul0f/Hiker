5def494e4309035f6792757967f6eb27cb5b76aa
Alexandr Voinkov <karauldouble@gmail.com>
2020-03-27 01:28:33 +0700

    object editor sketch

---
+++ ViewModels/ObjectViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Hiker_Editor.ViewModels
+{
+    class ObjectViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
