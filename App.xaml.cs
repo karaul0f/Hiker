b93b40cc3f7351f85a6d875bf88eba80c0a96b1b
Alexandr Voinkov <karauldouble@gmail.com>
2020-04-13 22:38:40 +0700

    localization files added

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Globalization;
 
 namespace Hiker_Editor
 {
@@ -13,6 +14,21 @@ namespace Hiker_Editor
     /// </summary>
     public partial class App : Application
     {
-        
-    }
+		private static List<CultureInfo> _languages = new List<CultureInfo>();
+
+		public static List<CultureInfo> Languages
+		{
+			get
+			{
+				return _languages;
+			}
+		}
+
+		public App()
+		{
+			_languages.Clear();
+			_languages.Add(new CultureInfo("en-US"));
+			_languages.Add(new CultureInfo("ru-RU"));
+		}
+	}
 }
