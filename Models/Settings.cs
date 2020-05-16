812f29118f6b97107187e6c27830e1e8b8ddaf95
Alexandr Voinkov <karauldouble@gmail.com>
2020-05-16 21:00:39 +0700

    Localization files updated(SpriteProperties, GameObjectProperties)

---
+++ Models/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiker_Editor.Models
+{
+    class Settings
+    {
+
+    }
+}
