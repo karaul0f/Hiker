69ef44327794e6a469c896645fc31662a27feb30
Alexandr Voinkov <colorx265@gmail.com>
2021-01-31 19:41:21 +0700

    Removed unnecessary projects

--- Runtime/Starter.cpp
+++ Runtime/Starter.cpp
@@ -1,13 +1,7 @@
 #include "stdafx.h"
-#include <cassert>
-#include "MainLoop.h"
 
 int main(int argc, char* argv[])
 {
-	HLOG("Program was started!\n");
-	assert(argc == 0);
-	Hiker::MainLoop::Instance()->Run(argv[0]);
-	HLOG("The program has been successfully completed\n");
 
 	return 0;
 }
